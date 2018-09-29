70bd93a4d0dfe318f58554ea6d73e94e464251a8
Shawn Ramsey <ramseys1990@gmail.com>
2018-09-29 16:28:19 -0400

    Adding in the custom cards for lovelace

---
+++ www/kiosk-config.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  LovelaceFullyKiosk.bind("3d471a9a-a92e6c63", "light.dashboard_screen", "binary_sensor.dashboard_motion");
+}, 200);
---
+++ www/lovelace-player-config.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+
+  // Add your bindings here
+  // Example:
+  // LovelacePlayer.bind("media_player.kitchen_dashboard", "ff90349d-4a6eb8b1");
+LovelacePlayer.bind("media_player.living_room_dashboard", "3d471a9a-a92e6c63");
+  // Remove this line when you found your Device ID
+  // LovelacePlayer.showDeviceId();
+}, 200);
